e6bc7ce4cd587d8466776e7957f5898a1484fffd
puertasman <puertasman@gmail.com>
2025-01-21 14:50:10 +0100

    casa mamma

---
+++ Basic/03-ejercicios.js
@@ -0,0 +1,52 @@
+// escribe un comentario de una línea
+// esto es un comentario de una línea
+
+// escribe un comentario de varias líneas
+/*
+Esto
+es
+un
+comentario
+multilínea
+*/
+// Declara variables con valores asociados a todos los datos de tipo primitivo
+let a = 0
+let b = 1.1
+let c = true
+let d = "cadena de texto"
+//falta el null, undefined, symbol y BigInt
+
+// imprime por consola el valor de todas las variables
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+// imprime por consola el tipo de todas las variables
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+
+// A continuación modifica los valores por otros del mismo tipo
+a = 3
+b = 1.5
+c = false
+d = "cadena diferente"
+
+// A continuación cambia por otro tipo de dato
+a = 2.3
+b = 3
+c = "esto ahora es una cadena"
+d = true
+
+// declara constantes con valores asociados a cada tipo de datos primitivos
+const E = 0
+const F = 1.1
+const G = true
+const H = "cadena de texto"
+
+// A continuación modifica valores de las constantes
+//E = 33
+
+// Comenta alguna línea que de error
---
+++ Basic/04-operadores.js
@@ -0,0 +1,50 @@
+// operadores
+
+// operadores aritméticos
+
+let a = 5
+let b = 10
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+
+console.log(a % b) // módulo
+console.log(a ** b) // Exponente
+
+a++ // incremento
+console.log(a)
+a-- // decremento 
+console.log(a)
+
+// operadores de asignación
+
+let myVariable = 2 // operador de asignación
+console.log(myVariable)
+myVariable += 2 // suma y asignación
+console.log(myVariable)
+myVariable -= 2 // resta y asignación
+console.log(myVariable)
+myVariable *= 4
+console.log(myVariable)
+myVariable /= 2
+console.log(myVariable)
+myVariable %= 3
+console.log(myVariable)
+myVariable **= 2
+console.log(myVariable)
+
+
+// operadores de comparación
+
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b)
+console.log(a <= b)
+console.log(a == b)
+console.log(a == 5)
+console.log(a == "5")
+console.log(a == a)
+console.log(a === 5)
+console.log(a === "5")
